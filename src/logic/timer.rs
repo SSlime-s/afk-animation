0af6bebc73d933669d34088dd7905397c4299a07
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2021-10-01 15:22:56 +0900

    ♻️ timer にテストを追加した

--- src/logic/timer.rs
+++ src/logic/timer.rs
@@ -1,4 +1,7 @@
-use chrono::{DateTime, Duration, Local, format::{DelayedFormat, StrftimeItems}};
+use chrono::{
+    format::{DelayedFormat, StrftimeItems},
+    DateTime, Duration, Local,
+};
 
 pub enum Timer {
     Measuring(DateTime<Local>),
@@ -56,5 +59,72 @@ impl Timer {
                 duration.num_milliseconds() % 1000 / 10
             )
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use chrono::{Local, TimeZone};
+
+    use super::*;
+
+    #[test]
+    fn test_measuring_start_format() {
+        let timer = Timer::Measuring(Local.ymd(2019, 5, 3).and_hms(13, 59, 20));
+        assert_eq!(
+            "05/03 13:59:20".to_string(),
+            timer.formatted_start().to_string(),
+        );
+    }
+
+    #[test]
+    fn test_ended_start_format() {
+        let timer = Timer::Ended(
+            Local.ymd(2019, 5, 3).and_hms(13, 59, 20),
+            Local.ymd(2021, 9, 4).and_hms(18, 43, 10),
+        );
+        assert_eq!(
+            "05/03 13:59:20".to_string(),
+            timer.formatted_start().to_string(),
+        );
+    }
+
+    #[test]
+    fn test_ended_end_format() {
+        let timer = Timer::Ended(
+            Local.ymd(2019, 5, 3).and_hms(13, 59, 20),
+            Local.ymd(2021, 9, 4).and_hms(18, 43, 10),
+        );
+        assert_eq!(
+            "09/04 18:43:10".to_string(),
+            timer.formatted_end().to_string(),
+        );
+    }
+
+    #[test]
+    fn test_ended_duration_format_over_1_hour() {
+        let timer = Timer::Ended(
+            Local.ymd(2019, 5, 3).and_hms(13, 50, 20),
+            Local.ymd(2019, 5, 3).and_hms(18, 53, 30),
+        );
+        assert_eq!("5h3m".to_string(), timer.formatted_duration(),);
+    }
+
+    #[test]
+    fn test_ended_duration_format_between_1_min_with_1_hour() {
+        let timer = Timer::Ended(
+            Local.ymd(2019, 5, 3).and_hms(13, 50, 20),
+            Local.ymd(2019, 5, 3).and_hms(13, 53, 30),
+        );
+        assert_eq!("3m10s".to_string(), timer.formatted_duration(),);
+    }
+
+    #[test]
+    fn test_ended_duration_format_under_1_min() {
+        let timer = Timer::Ended(
+            Local.ymd(2019, 5, 3).and_hms_milli(13, 50, 20, 490),
+            Local.ymd(2019, 5, 3).and_hms_milli(13, 51, 9, 601),
+        );
+        assert_eq!("49.11s".to_string(), timer.formatted_duration(),);
     }
 }
