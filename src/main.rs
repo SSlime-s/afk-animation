266b042291513b476672b18577f7938f1d460de0
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2021-10-01 14:40:45 +0900

    ♻️ `timer` を切り分けた

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
-use chrono::{
-    format::{DelayedFormat, StrftimeItems},
-    DateTime, Duration, Local,
-};
+mod logic;
+
 use rand::Rng;
 use std::{
     collections::VecDeque,
@@ -196,65 +194,6 @@ impl Lines {
     }
 }
 
-enum Timer {
-    Measuring(DateTime<Local>),
-    Ended(DateTime<Local>, DateTime<Local>),
-}
-impl Timer {
-    const TIME_FORMAT: &'static str = "%m/%d %H:%M:%S";
-
-    fn start() -> Self {
-        Self::Measuring(Local::now())
-    }
-
-    fn finish(&mut self) {
-        match self {
-            Self::Measuring(start_time) => *self = Self::Ended(*start_time, Local::now()),
-            Self::Ended(_, end_time) => *end_time = Local::now(),
-        }
-    }
-
-    fn formatted_start(&self) -> DelayedFormat<StrftimeItems<'_>> {
-        match self {
-            Self::Measuring(start_time) => start_time.format(Self::TIME_FORMAT),
-            Self::Ended(start_time, _) => start_time.format(Self::TIME_FORMAT),
-        }
-    }
-
-    fn formatted_end(&self) -> DelayedFormat<StrftimeItems<'_>> {
-        match self {
-            Self::Measuring(_) => Local::now().format(Self::TIME_FORMAT),
-            Self::Ended(_, end_time) => end_time.format(Self::TIME_FORMAT),
-        }
-    }
-
-    fn duration(&self) -> Duration {
-        match self {
-            Self::Measuring(start_time) => Local::now() - *start_time,
-            Self::Ended(start_time, end_time) => *end_time - *start_time,
-        }
-    }
-
-    fn formatted_duration(&self) -> String {
-        let duration = self.duration();
-        if duration.num_hours() > 0 {
-            format!("{}h{}m", duration.num_hours(), duration.num_minutes() % 60)
-        } else if duration.num_minutes() > 0 {
-            format!(
-                "{}m{}s",
-                duration.num_minutes(),
-                duration.num_minutes() % 60
-            )
-        } else {
-            format!(
-                "{}.{:>02}s",
-                duration.num_seconds(),
-                duration.num_milliseconds() % 1000 / 10
-            )
-        }
-    }
-}
-
 fn main() {
     assert!(COLOR_MIN < COLOR_MAX);
     assert_eq!((COLOR_MAX - COLOR_MIN) % COLOR_STEP, 0);
@@ -278,7 +217,7 @@ fn main() {
             .expect("Failed to set ctrlc handler");
     }
 
-    let mut timer = Timer::start();
+    let mut timer = crate::logic::timer::Timer::start();
 
     // hide cursor
     print!("\x1b[?25l");
