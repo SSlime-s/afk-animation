d15c4f7363cc0e9f7a38041b098bd70a14ecfb28
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2021-11-17 15:47:52 +0900

    🚑 linux 以外で動かなかった

--- src/logic/terminal.rs
+++ src/logic/terminal.rs
@@ -41,16 +41,8 @@ impl KeyManager {
     }
 
     fn get_terattr_from_os() -> libc::termios {
-        let mut attr = libc::termios {
-            c_iflag: 0,
-            c_oflag: 0,
-            c_cflag: 0,
-            c_lflag: 0,
-            c_cc: [0u8; 32],
-            c_ispeed: 0,
-            c_ospeed: 0,
-            c_line: 0,
-        };
+        let mut attr: libc::termios = unsafe { std::mem::zeroed() };
+
         unsafe {
             libc::tcgetattr(0, &mut attr);
         }
